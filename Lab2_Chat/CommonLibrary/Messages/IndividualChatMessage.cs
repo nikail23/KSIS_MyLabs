91508f83238309f22713efeb68c87abdc01e3368
nikail23 <nikail232323232323@gmail.com>
2020-03-28 14:18:15 +0200

    Moved the library to a higher level and added the serializer to the shared library

---
+++ Lab2_Chat/CommonLibrary/Messages/IndividualChatMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    class IndividualChatMessage : CommonChatMessage
+    {
+        public string receiverName { get; }
+        public IndividualChatMessage(DateTime dateTime, string senderIp, int senderPort, string senderName, string content, string receiverName) : base(dateTime, senderIp, senderPort, senderName, content)
+        {
+            this.receiverName = receiverName;
+        }
+    }
+}
